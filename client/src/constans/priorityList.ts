5cc5b1486e23ddeeee1137b9b6496117f83cfa8e
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-03-31 15:36:07 +0300

    feat: add priorityList

---
+++ client/src/constans/priorityList.ts
@@ -0,0 +1,20 @@
+import { PriorityOption } from '@/types';
+
+export const priorityList: PriorityOption[] = [
+  {
+    value: 'Low',
+    label: 'Low',
+  },
+  {
+    value: 'Medium',
+    label: 'Medium',
+  },
+  {
+    value: 'High',
+    label: 'High',
+  },
+  {
+    value: 'Critical',
+    label: 'Critical',
+  },
+];
