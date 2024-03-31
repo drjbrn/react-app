49120f4b61bb2b3ca193007ede09b1c657f77146
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-03-31 20:22:33 +0300

    feat: add loading message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import { useAppDispatch } from './store/hooks';
 import { TaskBoard } from './components/TaskBoard';
 import { getAllColumns } from './store/tasksColumn/tasksColumnThunks';
@@ -10,8 +10,18 @@ export const App = () => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(getAllTaskData());
-    dispatch(getAllColumns());
+    const fetchData = async () => {
+      try {
+        toast.info('Loading data...', { autoClose: false });
+        await dispatch(getAllColumns());
+        await dispatch(getAllTaskData());
+        toast.dismiss();
+      } catch (error) {
+        toast.error('An error occurred while loading data');
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
