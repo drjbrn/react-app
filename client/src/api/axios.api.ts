1c11903a23e39af20a61be179d7e43857ec79909
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-03-31 20:16:06 +0300

    fix: update api url

--- client/src/api/axios.api.ts
+++ client/src/api/axios.api.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+export const API_URL = import.meta.env.VITE_APP_API_URL
+  ? import.meta.env.VITE_APP_API_URL
+  : 'http://localhost:3000/api';
+
 export const instance = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_URL,
+  baseURL: API_URL,
 });
